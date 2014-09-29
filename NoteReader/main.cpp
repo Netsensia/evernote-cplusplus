f2944e8401f8b338b7d839e577ee767cc46e415e
Chris Moreton <chris@netsensia.com>
2014-09-29 21:39:58 +0100

    Initial Commit

---
+++ NoteReader/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NoteReader
+//
+//  Created by Chris Moreton on 29/09/2014.
+//  Copyright (c) 2014 Chris Moreton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
